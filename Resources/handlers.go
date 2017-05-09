dbf1161020ccc94b84e29b9cdfa28dee52f560da
Rashad Laher <rashadlaher@fsmc02jr0jgf96w.lan>
2017-05-09 13:45:19 -0500

    added issueparser handler

--- Resources/handlers.go
+++ Resources/handlers.go
@@ -2,12 +2,16 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/google/go-github/github"
 )
 
 //OrderInfo : update later with real order fields
 type OrderInfo struct {
-	Email   string `json:"email"`
+	Title   string `json:"title"`
+	Body    string `json:"body"`
 	Plasmid string `json:"plasmid"`
 	Strain  string `json:"strain"`
 }
@@ -23,4 +27,27 @@ func (order *OrderInfo) JDecoder(w http.ResponseWriter, r *http.Request) OrderIn
 		http.Error(w, "decoding error!", 400) //what number
 	}
 	return o
+}
+
+//IssueParser : converts orderinfo struct to github issue request
+//need to implement error handling
+func (order *OrderInfo) IssueParser(o OrderInfo) (github.IssueRequest, error) {
+	var issue github.IssueRequest
+	if o.Plasmid != "" && o.Strain != "" {
+		issue.Title = &o.Title
+		issue.Body = &o.Body
+		issue.Labels = &[]string{"Strain Order", "Plasmid Order"}
+	} else if o.Plasmid == "" && o.Strain != "" {
+		issue.Title = &o.Title
+		issue.Body = &o.Body
+		issue.Labels = &[]string{"Strain Order"}
+	} else if o.Plasmid != "" && o.Strain == "" {
+		issue.Title = &o.Title
+		issue.Body = &o.Body
+		issue.Labels = &[]string{"Plasmid Order"}
+	} else {
+		return issue, fmt.Errorf("no order in order")
+	}
+
+	return issue, nil
 }

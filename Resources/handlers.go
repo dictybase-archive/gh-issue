605f296b79b7f94545ba1d60a3324915873c9836
Rashad Laher <rashadlaher@fsmc02jr0jgf96w.lan>
2017-05-09 16:07:23 -0500

    updated json structs

--- Resources/handlers.go
+++ Resources/handlers.go
@@ -8,12 +8,61 @@ import (
 	"github.com/google/go-github/github"
 )
 
+type ShippingInfo struct {
+	Name       string `json:"name"`
+	University string `json:"university"`
+	Lab        string `json:"lab"`
+	Address    string `json:"address"`
+	Address2   string `json:"address2"`
+	Address3   string `json:"address3"`
+	Country    string `json:"country"`
+	Phone      string `json:"phone"`
+	Email      string `json:"email"`
+	Tracking   string `json:"tracking"`
+}
+
+type BillingInfo struct {
+	Name       string `json:"name"`
+	University string `json:"university"`
+	Lab        string `json:"lab"`
+	Address    string `json:"address"`
+	Address2   string `json:"address2"`
+	Address3   string `json:"address3"`
+	Country    string `json:"country"`
+	Phone      string `json:"phone"`
+	Email      string `json:"email"`
+	Payment    string `json:"payment"`
+}
+type Strains struct {
+	ID             string `json:"id"`
+	Descriptor     string `json:"descriptor"`
+	Name           string `json:"name"`
+	SystematicName string `json:"SystematicName"`
+}
+
+type Storage struct {
+	StoredAs string `json:"storedas"`
+	Location string `json:"location"`
+	NoVials  string `json:"novials"`
+	Color    string `json:"color"`
+}
+type Citations struct {
+	Citations string `json:"string"`
+}
+
+type Plasmids struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 //OrderInfo : update later with real order fields
 type OrderInfo struct {
-	Title   string `json:"title"`
-	Body    string `json:"body"`
-	Plasmid string `json:"plasmid"`
-	Strain  string `json:"strain"`
+	Owner      string `json:"owner"`
+	Repository string `json:"repository"`
+	Title      string `json:"title"`
+	Body       string `json:"body"`
+	Plasmid    string `json:"plasmid"`
+	Strain     string `json:"strain"`
 }
 
 //JDecoder : returns struct with relevant order fields

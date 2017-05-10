ece5295fd86c1ba79d431b82ca39d1b1db08b021
Rashad Laher <rashadlaher@fsmc02jr0jgf96w.lan>
2017-05-10 17:36:59 -0500

    workign on markdown, incomplete

--- Resources/handlers.go
+++ Resources/handlers.go
@@ -8,6 +8,11 @@ import (
 	"github.com/google/go-github/github"
 )
 
+type GeneralInfo struct {
+	Date    string `json:"date"`
+	OrderId string `json:"orderid"`
+}
+
 type ShippingInfo struct {
 	Name       string `json:"name"`
 	University string `json:"university"`
@@ -38,6 +43,8 @@ type Strains struct {
 	Descriptor     string `json:"descriptor"`
 	Name           string `json:"name"`
 	SystematicName string `json:"SystematicName"`
+	Storage        []Storage
+	Citations      []Citations
 }
 
 type Storage struct {
@@ -51,8 +58,10 @@ type Citations struct {
 }
 
 type Plasmids struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Storage   []Storage
+	Citations []Citations
 }
 
 //OrderInfo : update later with real order fields
@@ -60,9 +69,11 @@ type OrderInfo struct {
 	Owner      string `json:"owner"`
 	Repository string `json:"repository"`
 	Title      string `json:"title"`
-	Body       string `json:"body"`
-	Plasmid    string `json:"plasmid"`
-	Strain     string `json:"strain"`
+	GeneralInfo
+	ShippingInfo
+	BillingInfo
+	Strains  []Strains
+	Plasmids []Plasmids
 }
 
 //JDecoder : returns struct with relevant order fields
@@ -82,21 +93,35 @@ func (order *OrderInfo) JDecoder(w http.ResponseWriter, r *http.Request) OrderIn
 //need to implement error handling
 func (order *OrderInfo) IssueParser(o OrderInfo) (github.IssueRequest, error) {
 	var issue github.IssueRequest
-	if o.Plasmid != "" && o.Strain != "" {
-		issue.Title = &o.Title
-		issue.Body = &o.Body
+	issue.Title = &o.Title
+	//set the labels
+	if o.Plasmids != nil && o.Strains != nil {
 		issue.Labels = &[]string{"Strain Order", "Plasmid Order"}
-	} else if o.Plasmid == "" && o.Strain != "" {
-		issue.Title = &o.Title
-		issue.Body = &o.Body
+	} else if o.Plasmids == nil && o.Strains != nil {
 		issue.Labels = &[]string{"Strain Order"}
-	} else if o.Plasmid != "" && o.Strain == "" {
-		issue.Title = &o.Title
-		issue.Body = &o.Body
+	} else if o.Plasmids != nil && o.Strains == nil {
 		issue.Labels = &[]string{"Plasmid Order"}
 	} else {
 		return issue, fmt.Errorf("no order in order")
 	}
+	//generate markdown and put it in the body
+	//maybe try to use text/template package
+	/*"**Date:** {{.GeneralInfo.Date}}
+	  **Order ID:** {{GeneralInfo.OrderId}}"
+	  #Shipping and Billing Info#
+	| Shipping                  | Billing/Payer            |
+	|---------------------------|--------------------------|
+	| o.ShippingInfo.Name       | o.BillingInfo.Name       |
+	| o.ShippingInfo.University | o.BillingInfo.University |
+	| o.ShippingInfo.Lab        | o.BillingInfo.Lab        |
+	| o.ShippingInfo.Address    | o.BillingInfo.Address    |
+	| o.ShippingInfo.Address2   | o.BillingInfo.Address2   |
+	| o.ShippingInfo.Address3   | o.BillingInfo.Address3   |
+	| o.ShippingInfo.Country    | o.BillingInfo.Country    |
+	| o.ShippingInfo.Phone      | o.BillingInfo.Phone      |
+	| o.ShippingInfo.Email      | o.BillingInfo.Email      |
+	| o.ShippingInfo.Tracking   | o.BillingInfo.Payment    |
+	*/
 
 	return issue, nil
 }

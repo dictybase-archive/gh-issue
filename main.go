dbb2e342466273eed0301d91ed74b2f7bf426e12
Rashad Laher <rashadlaher@gmail.com>
2017-05-24 16:52:17 -0500

    basic implementation of server up and running

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"gh-issue/gh-issue/middlewares"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/dictyBase/gh-issue/commands"
+	"github.com/dictyBase/gh-issue/resources"
+	"github.com/dictyBase/go-middlewares/middlewares/chain"
 
 	"gopkg.in/urfave/cli.v1"
 )
@@ -53,9 +58,31 @@ func main() {
 			},
 		},
 		{
-			Name:   "run",
-			Usage:  "runs the server",
-			Action: commands.RunServer,
+			Name:  "run",
+			Usage: "runs the server",
+			Action: func(c *cli.Context) error {
+				var logMw *middlewares.Logger
+				if c.IsSet("log") {
+					w, err := os.Create(c.String("log"))
+					if err != nil {
+						log.Fatalf("cannot open log file %q\n", err)
+					}
+					defer w.Close()
+					logMw = middlewares.NewFileLogger(w)
+				} else {
+					logMw = middlewares.NewLogger()
+				}
+				mux := http.NewServeMux()
+
+				baseChain := chain.NewChain(logMw.LoggerMiddleware).ThenFunc(handlers.Placeholder)
+				//Chain := apollo.New(apollo.Wrap(logMw.LoggerMiddleware)).With(context.Background()).ThenFunc(handlers.Placeholder)
+				mux.Handle("/dicty/order", baseChain)
+				log.Printf("Starting web server on port %d\n", c.Int("port"))
+				log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", c.Int("port")), mux))
+				return nil
+			},
+
+			//commands.RunServer,
 			Flags: []cli.Flag{
 				cli.StringFlag{
 					Name:  "log,l",

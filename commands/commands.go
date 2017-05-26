f4073e92027225a37a323630d5441a9ccbe69987
Rashad Laher <rashadlaher@gmail.com>
2017-05-26 12:24:11 -0500

    getting runserver to work

--- commands/commands.go
+++ commands/commands.go
@@ -3,7 +3,13 @@ package commands
 import (
 	"context"
 	"fmt"
+	"gh-issue/gh-issue/middlewares"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/dictyBase/gh-issue/resources"
+	"github.com/dictyBase/go-middlewares/middlewares/chain"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 
@@ -39,5 +45,27 @@ func CreateIssue(c *cli.Context) error {
 		Body:  &body}
 
 	client.Issues.Create(ctx, owner, repository, &issue)
+	return nil
+}
+
+func RunServer(c *cli.Context) error {
+	var logMw *middlewares.Logger
+	if c.IsSet("log") {
+		w, err := os.Create(c.String("log"))
+		if err != nil {
+			log.Fatalf("cannot open log file %q\n", err)
+		}
+		defer w.Close()
+		logMw = middlewares.NewFileLogger(w)
+	} else {
+		logMw = middlewares.NewLogger()
+	}
+	mux := http.NewServeMux()
+
+	baseChain := chain.NewChain(logMw.LoggerMiddleware).ThenFunc(handlers.Placeholder)
+	//Chain := apollo.New(apollo.Wrap(logMw.LoggerMiddleware)).With(context.Background()).ThenFunc(handlers.Placeholder)
+	mux.Handle("/dicty/order", baseChain)
+	log.Printf("Starting web server on port %d\n", c.Int("port"))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", c.Int("port")), mux))
 	return nil
 }

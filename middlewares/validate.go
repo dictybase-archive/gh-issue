fa874e04ac679508dbc75d91de5973aabc7ccde2
Rashad Laher <rashadlaher@gmail.com>
2017-06-02 13:32:36 -0500

    working on validation

---
+++ middlewares/validate.go
@@ -0,0 +1,30 @@
+package validate
+
+import (
+	"context"
+	"gh-issue/gh-issue/models"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/manyminds/api2go/jsonapi"
+)
+
+func JsonValidator(fn http.HandlerFunc) http.HandlerFunc {
+	var order models.Orderinfo
+
+	newfn := func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "error reading Body", http.StatusInternalServerError)
+		}
+		err = jsonapi.Unmarshal(body, &order)
+		if err != nil {
+			http.Error(w, "error unmarshaling json struct", http.StatusInternalServerError)
+
+		}
+		ctx := context.WithValue(r.Context(), "DecodedJson", order)
+		fn(w, r.WithContext(ctx))
+	}
+	return newfn
+
+}

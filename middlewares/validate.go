a3977a073f1bce0fd86c36c89cb2a31ece20bbfb
Rashad Laher <rashadlaher@gmail.com>
2017-06-20 11:25:14 -0500

    middleware errors now break chain #16

--- middlewares/validate.go
+++ middlewares/validate.go
@@ -17,12 +17,12 @@ func JSONValidator(fn http.HandlerFunc) http.HandlerFunc {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-
+			return
 		}
 		err = jsonapi.Unmarshal(body, &order)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-
+			return
 		}
 		ctx := context.WithValue(r.Context(), "DecodedJson", order)
 		fn(w, r.WithContext(ctx))

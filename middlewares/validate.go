9ce8043c03e58efc0614a94d53b998258b1dcc8e
Rashad Laher <rashadlaher@gmail.com>
2017-06-21 15:00:32 -0500

    new unit test halfway there

--- middlewares/validate.go
+++ middlewares/validate.go
@@ -2,7 +2,6 @@ package validate
 
 import (
 	"context"
-	"io"
 	//"github.com/dictyBase/gh-issue/models"
 	"gh-issue/gh-issue/models"
 	"io/ioutil"
@@ -12,34 +11,35 @@ import (
 )
 
 //TEMP
-func Temp(w http.ResponseWriter, r *http.Request) {
-	//var order models.Orderinfo
-	io.WriteString(w, `{"alive": true}`)
-	body, err := ioutil.ReadAll(r.Body)
-	if body != nil {
-		http.Error(w, "body not nil", http.StatusInternalServerError)
-		return
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	// err = jsonapi.Unmarshal(body, &order)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
-}
+// func Temp(w http.ResponseWriter, r *http.Request) {
+// 	//var order models.Orderinfo
+// 	body, err := ioutil.ReadAll(r.Body)
+// 	if body != nil {
+// 		http.Error(w, "body not nil", http.StatusInternalServerError)
+// 		return
+// 	}
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
 
-func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	// A very simple health check.
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
+// err = jsonapi.Unmarshal(body, &order)
+// if err != nil {
+// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+// 	return
+// }
+//}
 
-	// In the future we could report back on the status of our DB, or our cache
-	// (e.g. Redis) by performing a simple PING, and include them in the response.
-	io.WriteString(w, `{"alive": true}`)
-}
+//
+// func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+// 	// A very simple health check.
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Header().Set("Content-Type", "application/json")
+//
+// 	// In the future we could report back on the status of our DB, or our cache
+// 	// (e.g. Redis) by performing a simple PING, and include them in the response.
+// 	io.WriteString(w, `{"alive": true}`)
+// }
 
 //JSONValidator breaks chain if JSON is not valid and passes the decodedJSON on through context
 func JSONValidator(fn http.HandlerFunc) http.HandlerFunc {
--- middlewares/validate_test.go
+++ middlewares/validate_test.go
@@ -2,7 +2,6 @@ package validate
 
 import (
 	"bytes"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -39,36 +38,28 @@ import (
 // 	}
 // }
 
+func temp(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("body string"))
+	w.WriteHeader(200)
+}
+
 func TestJSONValidator(t *testing.T) {
-	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-	// pass 'nil' as the third parameter.
-	test := "test"
-	b := bytes.NewBufferString(test)
+
+	testHandlerFn := http.HandlerFunc(temp)
+	w := httptest.NewRecorder()
+
+	b := bytes.NewBufferString("test22")
 	req, err := http.NewRequest("POST", "/json-test", b)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	io.WriteString(rr, "test")
-	handler := http.HandlerFunc(Temp)
-
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
+	JSONValidator(testHandlerFn).ServeHTTP(w, req)
 
 	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
+	if status := w.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
-	}
-
-	// Check the response body is what we expect.
-	expected := `{"alive": true}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
 	}
 
 }
